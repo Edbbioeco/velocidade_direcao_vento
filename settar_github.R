ebfff16c2382e4f2d99455b04e6865c65455b479
Edbbioeco <edsonbbiologia@gmail.com>
2026-08-28 18:55:19 -0300

    Criar repositório

---
+++ settar_github.R
@@ -0,0 +1,19 @@
+# PAcotes ----
+
+library(usethis)
+
+library(gert)
+
+# Iniciar git ----
+
+usethis::use_git()
+
+# Primeiro commit ----
+
+gert::git_add(files = ".gitignore")
+
+gert::git_commit(message = "gitignore")
+
+# Criar repositório ----
+
+usethis::use_github()
