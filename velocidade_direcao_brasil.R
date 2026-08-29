f1273aa0acce6151a1271887ec462b4217db3019
Edbbioeco <edsonbbiologia@gmail.com>
2026-08-29 01:22:13 -0300

    Alterar nome das camadas

--- velocidade_direcao_brasil.R
+++ velocidade_direcao_brasil.R
@@ -140,7 +140,12 @@ raster_vento_trat_vel <- purrr::map(
 
     direcao <- (270 - atan2(raster$v10, raster$u10) * 180/pi) %% 360
 
-    c(velocidade, direcao)
+    raster <- c(velocidade, direcao)
+
+    names(raster) <- c("Velocidade (m/s)",
+                       "Direção (°)")
+
+    raster
 
     },
   .progress = TRUE)
