3f36b93e7cafcadcf7e39536eb29d6472c87bf21
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-10 19:31:45 -0300

    Settar o Token do CDS

--- velocidade_direcao_por_hora.R
+++ velocidade_direcao_por_hora.R
@@ -34,3 +34,9 @@ ggplot() +
 br_bbox <- br |> sf::st_bbox()
 
 br_bbox
+
+# Baixar rasters de velocidade e diração do vento ----
+
+## Autenticar token ----
+
+ecmwfr::wf_set_key(key = Sys.getenv("CDS_TOKEN"))
